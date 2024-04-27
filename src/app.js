144f2c2cd6d3a023c7d7e95962edc64ae229e898
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-04-27 19:39:16 +0000

    removed comments to allow pre-loaded code to run

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
 /* eslint-disable */
-/*import "bootstrap";
+import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};*/
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed on', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  function excuse(param1, param2, param3, param4) {
+    let who = param1[Math.floor(Math.random() * 4)];
+    let action = param2[Math.floor(Math.random() * 4)];
+    let what = param3[Math.floor(Math.random() * 3)];
+    let when = param4[Math.floor(Math.random() * 5)];
+    return who + " " + action + " " + what + " " + when;
+  }
 
-function excuse(param1, param2, param3, param4) {
-  let who = param1[Math.floor(Math.random() * 4)];
-  let action = param2[Math.floor(Math.random() * 4)];
-  let what = param3[Math.floor(Math.random() * 3)];
-  let when = param4[Math.floor(Math.random() * 5)];
-  return who + " " + action + " " +  what + " " + when
-}
-
-document.write(excuse(who, action, what, when));
+  console.log(excuse(who, action, what, when));
+};
